d5c1f030874e61941ee852833f78df7e996eca29
ImaginePilot <zhaoyang.lynn.xie@gmail.com>
2024-12-01 18:23:24 -0500

    uploaded themes and colors

--- app/src/main/java/com/Plantze/tracker/HomeMenu.java
+++ app/src/main/java/com/Plantze/tracker/HomeMenu.java
@@ -5,19 +5,22 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class HomeMenu extends AppCompatActivity{
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HomeMenu extends AppCompatActivity {
     private Button annualCarbonButton;
     private Button ecoTrackerButton;
     private Button ecoGuageButton;
     private Button ecoBalanceButton;
     private Button ecoHubButton;
     private Button ecoAgentButton;
+    private Button ecotrackerButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home_page);
-        annualCarbonButton = findViewById(R.,id.annualCarbonButton);
+        setContentView(R.layout.home_page);
+        annualCarbonButton = findViewById(R.id.annualCarbonButton);
         ecotrackerButton = findViewById(R.id.ecoTrackerButton);
         ecoGuageButton = findViewById(R.id.ecoGuageButton);
         ecoHubButton = findViewById(R.id.ecoHubButton);
@@ -26,24 +29,24 @@ public class HomeMenu extends AppCompatActivity{
         annualCarbonButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(HomeMenu.this, );
-                startActivity(intent);
+                //Intent intent = new Intent(HomeMenu.this, );
+                //startActivity(intent);
             }
         });
 
-        ecoTrackerButton.setClickListener(new View.OnClickListener(){
+        ecoTrackerButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                Intent intent = new Intent(HomeMenu.this, );
-                startActivity(intent);
+                //Intent intent = new Intent(HomeMenu.this, );
+                //startActivity(intent);
             }
         });
 
         ecoGuageButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                Intent intent = new Intent(HomeMenu.this, );
-                startActivity(intent);
+                //Intent intent = new Intent(HomeMenu.this, );
+                //startActivity(intent);
             }
         });
     }
