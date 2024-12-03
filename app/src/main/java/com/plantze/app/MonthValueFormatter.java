7cd532291da0e7c8d9dd5a966b6393f382e7e1d6
Jiacheng <ljc050414@gmail.com>
2024-12-02 22:54:18 -0500

    changed app's name and add Linechart activity

---
+++ app/src/main/java/com/plantze/app/MonthValueFormatter.java
@@ -0,0 +1,23 @@
+package com.plantze.app;
+
+import com.github.mikephil.charting.formatter.ValueFormatter;
+
+/**
+ * This class is the formatter for numbers from 0-11 to Jan-Dec
+ */
+public class MonthValueFormatter extends ValueFormatter{
+    private final String[] months = {
+        "JAN", "FEB", "MAR", "APR", "MAY", "JUN",
+                "JUL", "AUG", "SEP", "OCT", "NOV","DEC"
+    }; //Shorthand of months;
+
+    @Override
+    public String getFormattedValue(float value) {
+        int index = (int) value;
+        if (index >= 0 && index < 12) {
+            return months[index];
+        } else {
+            return "";
+        }
+    }
+}
--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,6 @@
 plugins {
 
     id("com.google.gms.google-services") version "4.4.2" apply false
-    id("com.android.application") version "8.2.1" apply false
-    id("com.android.library") version "8.2.1" apply false
+    id("com.android.application") version "8.6.0" apply false
+    id("com.android.library") version "8.6.0" apply false
 }
